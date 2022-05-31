6e39b2c394f776da896585913d46c1c0397be6c1
Wezley Singleton <wezley.singleton@gmail.com>
2022-05-31 18:05:35 -0400

    basic sanity servlet implemented

---
+++ src/main/java/com/revature/quizzard/servlets/SanityServlet.java
@@ -0,0 +1,26 @@
+package com.revature.quizzard.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+public class SanityServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        System.out.println("[LOG] - SanityServlet received a request at " + LocalDateTime.now());
+        System.out.println("[LOG] - Request URI: " + req.getRequestURI());
+        System.out.println("[LOG] - Request method: " + req.getMethod());
+        System.out.println("[LOG] - Request header, example: " + req.getHeader("example"));
+
+        resp.setStatus(200); // it is 200 by default
+        resp.setHeader("Content-type", "text/plain");
+        resp.setHeader("example-response-header", "some-example-value");
+        resp.getWriter().write("This is the response payload");
+
+    }
+}
