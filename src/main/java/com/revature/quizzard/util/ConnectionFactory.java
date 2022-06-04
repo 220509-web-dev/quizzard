e6ca097514a10c750bab23a9f5b388a0e9b01549
Wezley Singleton <wezley.singleton@gmail.com>
2022-06-04 17:38:14 -0400

    updated props loader to use more portable syntax

--- src/main/java/com/revature/quizzard/util/ConnectionFactory.java
+++ src/main/java/com/revature/quizzard/util/ConnectionFactory.java
@@ -32,7 +32,7 @@ public class ConnectionFactory {
 
     private ConnectionFactory() {
         try {
-            props.load(new FileReader("src/main/resources/application.properties"));
+            props.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("application.properties"));
         } catch (Exception e) {
             System.err.println("Failed to load database credentials from property file.");
             throw new RuntimeException(e); // fail fast for easier debugging
